06c646f3f7e87ad93e48e1ed7f783359269a490a
Cichol <cichol@live.cn>
2016-07-12 18:45:32 +0800

    update CRUD

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -9,14 +9,27 @@ class CharactersController < ApplicationController
   end
 
   def edit
-
+    @character = Character.find(params[:id])
   end
 
   def update
+    @character = Character.find(params[:id])
+    @character.update(character_params)
+    redirect_to character_path(@character)
+  end
 
+  def new
+    @character = Character.new
   end
 
   def create
-
+    @character = Character.create(character_params)
+    redirect_to character_path(@character)
   end
+
+  private
+
+    def character_params
+      params.require(:character).permit(:name, :brief, :description)
+    end
 end
--- app/controllers/concepts_controller.rb
+++ app/controllers/concepts_controller.rb
@@ -8,14 +8,27 @@ class ConceptsController < ApplicationController
   end
 
   def edit
-
+    @concept = Concept.find(params[:id])
   end
 
   def update
+    @concept = Concept.find(params[:id])
+    @concept.update(concept_params)
+    redirect_to concept_path(@concept)
+  end
 
+  def new
+    @concept = Concept.new
   end
 
   def create
-
+    @concept = Concept.create(concept_params)
+    redirect_to concept_path(@concept)
   end
+
+  private
+
+    def concept_params
+      params.require(:concept).permit(:name, :description)
+    end
 end
